b01a5b5c56758eaf67a4bf1c2d233ac0e5f313d2
sybida <422434393@qq.com>
2020-12-26 15:01:24 +0800

    改善单个班级

--- student/js/classStudent.js
+++ student/js/classStudent.js
@@ -1,6 +1,8 @@
 var pageNum = 1
-var pageSize = $("#pageSizeSel").val()
-var classId
+var pageSize = 5
+var classId=1
+
+var userid=getCookie("userid")
 // var teacherStudy=-1
 var search = location.search
 var arr = search.split("&")
@@ -12,34 +14,20 @@ if (arr.length > 1) {
     pageSize = pageSize1.split("=")[1]
     //
     userid1=arr[2]
-    userid=userid1.split("=")[1]
+    classId=userid1.split("=")[1]
 }
-var selectA2 = $("#pageSizeSel").find("option"); //从A1下拉框中 搜索值
-for (var i = 0; i < selectA2.length; i++) {
-    var t = $(selectA2[i]).val()
 
-    if (t == pageSize) {
-        $(selectA2[i]).attr("selected", "selected")
-    }
 
-}
-//每页条数的change事件
-$('#pageSizeSel').change(
-    function () {
-        pageSize = $("#pageSizeSel").val()
-    }
-)
 
 $("#selectButt").click(function () {
-
-    location.href = "/sybida/student/classStudent.html?pageNum=1&pageSize="+pageSize+"&userId="+userid
+    location.href = "/sybida/student/classStudent.html?pageNum=1&pageSize="+pageSize+"&classId="+classId
 })
 
 
 $("#selectButt1").click(function () {
     let stuName = $("#searcheStuId").val()
     if (!stuName || !(stuName.trim())) {
-        location.href = "/sybida/student/classStudent.html?pageNum=1&pageSize="+pageSize+"&userId="+userid
+        location.href = "/sybida/student/classStudent.html?pageNum=1&pageSize="+pageSize+"&classId="+classId
     } else {
             showDetail(stuName)
     }
@@ -49,82 +37,7 @@ layui.use('layer', function(){
     layer = layui.layer;
 });
 
-var userid=getCookie("userid")
-function showDetail(stuName) {
-    $.getJSON({url:url + "/student/selectstudentbyname", data:"stuName=" + stuName+"&classId="+classId,
-        beforeSend: function(request) {
-            request.setRequestHeader("token", userid);
-        },
-        success:function (data2) {
-            if(data== -1000){
-                location.href=logindexurl
-            }else{
-                if (data2.code == 0){
-                    layer.alert("学生不存在", {
-                            skin: 'layui-layer-molv' //样式类名
-                            ,closeBtn: 0
-                        }
-                    );
-                }else {
-                    $("#detailCon").css('display', '')
-                    let html = ''
-                    console.log(data2)
-                    let list = data2.data[0]
-                    if (!list.studentSex) {
-                        list.studentSex = "暂无"
-                    }
-                    if (!list.studentIdentity) {
-                        list.studentIdentity = "暂无"
-                    }
-                    if (!list.studentIsGraduation) {
-                        list.studentIsGraduation = "暂无"
-                    }
-                    if (!list.studentSchool) {
-                        list.studentSchool = "暂无"
-                    }
-                    if (!list.studentSpecialty) {
-                        list.studentSpecialty = "暂无"
-                    }
-                    if (!list.studentWechat) {
-                        list.studentWechat = "暂无"
-                    }
-                    if (!list.studentParentPhone) {
-                        list.studentParentPhone = "暂无"
-                    }
-                    if (!list.studentParentName) {
-                        list.studentParentName = "暂无"
-                    }
-                    if (!list.studentClassId) {
-                        list.studentClassId = "暂无"
-                    }
-                    if (!list.studentPhone) {
-                        list.studentPhone = "暂无"
-                    }
-                    if (!list.studentCity) {
-                        list.studentCity = "暂无"
-                    }
 
-                    html += `<tr class="warning">
-                             <td>${list.studentId}</td>
-                             <td>${list.studentName}</td>
-                             <td>${list.studentSex}</td>
-                             <td>${list.studentIdentity}</td>
-                             <td>${list.studentIsGraduation}</td>
-                             <td>${list.studentSchool}</td>
-                             <td>${list.studentSpecialty}</td>
-                             <td>${list.studentWechat}</td>
-                             <td>${list.studentCity}</td>
-                             <td>${list.studentParentPhone}</td>
-                             <td>${list.studentParentName}</td>
-                             <td>${list.studentPhone}</td>
-                             <td>${list.studentClassId}</td><td></td></tr>`
-                    $("#detailedInfo").append(html)
-                }
-
-            }
-
-    }})
-}
 
 
 
@@ -132,16 +45,50 @@ function showDetail(stuName) {
 
 
 console.log(userid+"==========================")
+//根据权限查询班级
+$.getJSON({
+    url: url+"/classInfo/selectteachermuticlass",
+    data:"userId="+userid,
+    beforeSend: function(request) {
+        request.setRequestHeader("token", userid);
+    },
+    success: function(data) {
+        if(data== -1000){
+            location.href=logindexurl
+        }else{
+            console.log(data)
+            var html = `<option value="-1">请选择</option>`
+            for (let i = 0; i < data.data.length; i++) {
+                html += `<option value="${data.data[i].classId}">${data.data[i].classNum}</option>`
+            }
+            $('#classNum').append(html);
+            var selectA1 = $('#classNum').find("option"); //从A1下拉框中 搜索值
+            for (var i = 0; i < selectA1.length; i++) {
+                var t=$(selectA1[i]).val()
+                console.log(t+"===============")
+                if(t==classNum){
+                $(selectA1[i]).attr("selected", "selected")
+                }
+            }
+            //change事件
+            $('#classNum').change(function () {
+                    classId = $('#classNum').val()
+                }
+            )
+        }}
+});
+
+
+console.log(classId+"=111111111111211111111111111")
+
 show();
-
-
 function show() {
 
     $.ajax({
-        url: url+"classInfo/teacheridfindclass",
+        url: url+"classInfo/teacheridfindclass2",
         type: 'POST',
         //   timeout: '30000',
-        data: "pageSize=" + pageSize + "&pageNum=" + pageNum+"&userId="+userid,
+        data: "pageSize=" + pageSize + "&pageNum=" + pageNum+"&classId="+classId,
         // contentType: 'application/json;charset=utf-8',
         dataType: 'json',
 
@@ -152,6 +99,7 @@ function show() {
             if(data== -1000){
                 location.href=logindexurl
             }else{
+                console.log(data)
                 if(data.code==1) {
                     let html = ''
                     console.log(data)
@@ -255,16 +203,60 @@ function show() {
                             location.href="/sybida/student/updateInfoStudent.html?id="+id;
                         }
                     })
-                    classId=data.message
-                    $("#abtn").attr("href","http://localhost:8080/teacher/selectstudentclassid?classId="+classId);
+                    // classId=data.message
+                    // $("#abtn").attr("href","http://localhost:8080/teacher/selectstudentclassid?classId="+classId);
+                    $("#abtn").click(function () {
+                        console.log("======================")
+                        console.log(classId+"------------------------------")
+                        $.getJSON({
+                            url: url+"/teacher/selectstudentclassid",
+                            data:"classId="+classId,
+                            beforeSend: function(request) {
+                                request.setRequestHeader("token", userid);
+                            },
+                            success:function (data) {
+                                if(data== -1000){
+                                    location.href=logindexurl
+                                }else{
+                                    if (data.code==1){
+                                        var beforeData = data.data
+                                        let fields=["学生ID","学生姓名","学生性别","身份证号","是否毕业","毕业学校","专业",
+                                            "学生微信","邮箱","学习方向","就业城市","家庭住址","家长电话","家长姓名","紧急电话",
+                                        "学生电话","学生班级","学生寝室","学生头像","修改时间","评级","是否删除"]
+                                        let afterData = beforeData.map((item)=>{
+                                            return{
+                                                "学生ID" : item.studentId, "学生姓名":item.studentName,
+                                                "学生性别":item.studentSex,"身份证号":item.studentIdentity,
+                                                "是否毕业":item.studentIsGraduation,"毕业学校":item.studentSchool,
+                                                "专业":item.studentSpecialty,"学生微信":item.studentWechat,
+                                                "邮箱":item.studentMailbox,"学习方向":item.studentStudyId,
+                                                "就业城市":item.studentCity,"家庭住址":item.studentAddress,
+                                                "家长电话":item.studentParentPhone,"家长姓名":item.studentParentName,
+                                                "紧急电话":item.studentUrgent,"学生电话":item.studentPhone,
+                                                "学生班级":item.studentClassId,"学生寝室":item.studentRoom,
+                                                "学生头像":item.studentPhoto,"修改时间":item.studentAlterTime,
+                                                "修改时间":item.studentAlterTime,"评级":item.studentNull1,
+                                                "是否删除":item.studentNull2
+                                            }
+                                        })
+
+
+                                        if (typeof XLSX == 'undefined') XLSX = require('xlsx');
+                                        let ws = XLSX.utils.json_to_sheet(afterData);
+                                        let wb = XLSX.utils.book_new();
+                                        XLSX.utils.book_append_sheet(wb, ws, "People");
+                                        XLSX.writeFile(wb, "学生" + ".xlsx");
+                                    }
+                                }
+                            }
+                        })
+                    })
+
+
                 }
             }
 
             }
-
-
-
-
     })
 }
 
@@ -292,20 +284,20 @@ function pageSelect(data){
         // classStudent.html?pageNum=1&pageSize="+pageSize+"&userId="+userid
         html+=`<li class="disabled"><a href="#" aria-label="Previous">&laquo;</a></li>`
     }else{
-        html+=`<li><a href="/sybida/student/classStudent.html?pageNum=${pageNum-1}&pageSize=${pageSize}&userId=${userid}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>`
+        html+=`<li><a href="/sybida/student/classStudent.html?pageNum=${pageNum-1}&pageSize=${pageSize}&classId=${classId}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>`
     }
     for(var i=data.navigateFirstPage;i<=data.navigateLastPage;i++){
         if(pageNum==i){
             html+=`<li class="active"><a href="#">${i}</a></li>`
         }else{
-            html+=`<li><a href="/sybida/student/classStudent.html?pageNum=${i}&pageSize=${pageSize}&userId=${userid}">${i}</a></li>`
+            html+=`<li><a href="/sybida/student/classStudent.html?pageNum=${i}&pageSize=${pageSize}&classId=${classId}">${i}</a></li>`
         }
     }
     if((data.pages)<=pageNum){
         html+=`<li class="disabled"><a href="#">&raquo;</a></li>`
     }else{
         var pa=parseInt(pageNum)+1
-        html+=`<li><a href="/sybida/student/classStudent.html?pageNum=${pa}&pageSize=${pageSize}&userId=${userid}" aria-label="Next"> <span aria-hidden="true">&raquo;</span></a></li>`
+        html+=`<li><a href="/sybida/student/classStudent.html?pageNum=${pa}&pageSize=${pageSize}&classId=${classId}" aria-label="Next"> <span aria-hidden="true">&raquo;</span></a></li>`
     }
     html+=`<span style="font-family: '微软雅黑';font-size: 15px;margin-left:200px;padding-top: 10px;line-height: 40px">共&nbsp;${data.pages}&nbsp;页</span>`
     $("#pagination").append(html)
@@ -351,5 +343,81 @@ $("#deleteOneSure").click(function (){
 
 $("#closebtn").click(function () {
     $("#detailCon").css('display', 'none')
-    location.href = "/sybida/student/classStudent.html?pageNum=1&pageSize="+pageSize+"&userId="+userid
+    location.href = "/sybida/student/classStudent.html?pageNum=1&pageSize="+pageSize+"&classId="+classId
 })
+
+function showDetail(stuName) {
+    $.getJSON({url:url + "/student/selectstudentbyname", data:"stuName=" + stuName+"&classId="+classId,
+        beforeSend: function(request) {
+            request.setRequestHeader("token", userid);
+        },
+        success:function (data2) {
+            if(data2== -1000){
+                location.href=logindexurl
+            }else{
+                if (data2.code == 0){
+                    layer.alert("学生不存在", {
+                            skin: 'layui-layer-molv' //样式类名
+                            ,closeBtn: 0
+                        }
+                    );
+                }else {
+                    $("#detailCon").css('display', '')
+                    let html = ''
+                    console.log(data2)
+                    let list = data2.data[0]
+                    if (!list.studentSex) {
+                        list.studentSex = "暂无"
+                    }
+                    if (!list.studentIdentity) {
+                        list.studentIdentity = "暂无"
+                    }
+                    if (!list.studentIsGraduation) {
+                        list.studentIsGraduation = "暂无"
+                    }
+                    if (!list.studentSchool) {
+                        list.studentSchool = "暂无"
+                    }
+                    if (!list.studentSpecialty) {
+                        list.studentSpecialty = "暂无"
+                    }
+                    if (!list.studentWechat) {
+                        list.studentWechat = "暂无"
+                    }
+                    if (!list.studentParentPhone) {
+                        list.studentParentPhone = "暂无"
+                    }
+                    if (!list.studentParentName) {
+                        list.studentParentName = "暂无"
+                    }
+                    if (!list.studentClassId) {
+                        list.studentClassId = "暂无"
+                    }
+                    if (!list.studentPhone) {
+                        list.studentPhone = "暂无"
+                    }
+                    if (!list.studentCity) {
+                        list.studentCity = "暂无"
+                    }
+
+                    html += `<tr class="warning">
+                             <td>${list.studentId}</td>
+                             <td>${list.studentName}</td>
+                             <td>${list.studentSex}</td>
+                             <td>${list.studentIdentity}</td>
+                             <td>${list.studentIsGraduation}</td>
+                             <td>${list.studentSchool}</td>
+                             <td>${list.studentSpecialty}</td>
+                             <td>${list.studentWechat}</td>
+                             <td>${list.studentCity}</td>
+                             <td>${list.studentParentPhone}</td>
+                             <td>${list.studentParentName}</td>
+                             <td>${list.studentPhone}</td>
+                             <td>${list.studentClassId}</td><td></td></tr>`
+                    $("#detailedInfo").append(html)
+                }
+
+            }
+
+        }})
+}
